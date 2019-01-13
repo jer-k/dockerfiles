adb074a2abffa2c5ef9405201d1b2b4dc41933ec
Jeremy Kreutzbender <jkreutzbender@truecar.com>
2019-01-13 15:26:49 -0800

    Update script

--- shared/wait_for_pg.sh
+++ shared/wait_for_pg.sh
@@ -4,12 +4,20 @@ set -e
 
 host="$1"
 shift
-cmd="$@"
 
-until psql -h "$host" -U "postgres" -c '\l'; do
+end_time=$(( $(printf '%(%s)T' -1) + 60 ))
+while :; do
+  psql -h "$host" -U "postgres" -c '\l' 2>&1 && break
+
+  if (( $(printf '%(%s)T' -1) >= $end_time )); then
+    echo >&2 'Taking too long to connect to Postgres, giving up'
+    exit 1
+  fi
+
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
+  sleep 3
 done
 
->&2 echo "Postgres is up - executing command"
-exec $cmd
+>&2 echo "Postgres is up - executing command: '$@'"
+exec "$@"
+
